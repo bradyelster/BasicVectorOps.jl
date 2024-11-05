7533d2b960a7be317ff8578013455b8ee46380e9
Documenter.jl <documenter@juliadocs.github.io>
2024-11-05 02:15:14 +0000

    build based on 7882dba

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BasicVectorOps","category":"page"},{"location":"#BasicVectorOps","page":"Home","title":"BasicVectorOps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BasicVectorOps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BasicVectorOps]","category":"page"}]
+}
