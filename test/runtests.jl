dcc1553ea2d9194b1a8ea565ae79f0ec6e514ef7
bradypc <belster@auburn.edu>
2024-11-04 20:39:06 -0600

    improved functionality for vectorMag

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "BasicVectorOps.jl" begin
     # Write your tests here.
     vectorMag([1, 1, 1]) == √3
-    vectorMag([1, 2, 1]) == 3.0
+    vectorMag([2, 1, 2]) == 3.0
     vectorMag([0, 0, 0]) == 0.0
-    vectorMag([1//2, 1//1, 1//2]) == √3 / 2
+    vectorMag([1//2, 1//1, 1//2]) == √(3 / 2)
 end
