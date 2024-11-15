1b1e465c8628576b9357ea5c752e4a71a98a3b52
bradypc <belster@auburn.edu>
2024-11-14 22:46:01 -0600

    removed redundant lines in magnitude.jl

--- src/magnitude.jl
+++ src/magnitude.jl
@@ -1,28 +1,27 @@
-@doc """
-Compute the magnitude of a given vector.
+@doc
 """
-function vectorMag(v::Vector)
-    return (sqrt ∘ sum)(v .^ 2)
-    # could also write:
-    # v.^2 |> sum |> sqrt
-    # or simply
-    # sqrt(sum(v.^2))
-end
+### vectorMag
+`vectorMag` compute the magnitude of a given input vector.
+"""
+vectorMag(v::Vector) = (sqrt ∘ sum)(v .^ 2)
 
-@doc """
-Compute the dot product between two vectors.
-""" ->
-function vectorDot(v::Vector, w::Vector)::Float64
-    return Float64(v' * w)
-end
+@doc
+"""
+### vectorDot 
+`vectorDot` computes the dot product between two input vectors.
+"""
+vectorDot(v::Vector, w::Vector)::Float64 = Float64(v' * w)
 
-@doc """
-Find the angle between the vector and the positive x-axis. 
-Output angle can be in either radians or degrees, specified by the 'mode' 
-and is rounded to the nearest decimal.
-""" ->
+
+@doc
+"""
+### findAngle
+`findAngle` finds the angle between the vector and the positive x-axis. 
+Output angle can be in either radians or degrees, specified 
+by the Boolean `degMode`` and is rounded to the nearest decimal.
+"""
 function findAngle(v::Vector, degMode = false)
-    return length(v) < 2 ? error("input vector must be 2-dimensional") :
-        degMode == true ? round(atand(v[2], v[1]), digits = 1) :
+    return length(v) < 2 ? error("Input vector must be 2-dimensional") :
+        degMode ? round(atand(v[2], v[1]), digits = 1) :
         round(atan(v[2], v[1]), digits = 1)
 end
