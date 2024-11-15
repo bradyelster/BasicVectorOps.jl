e2c528cfe7e53b01b01fd628aaa807e199e6c7f1
bradypc <belster@auburn.edu>
2024-11-14 22:29:34 -0600

    optimized control flow for findAngle

--- src/magnitude.jl
+++ src/magnitude.jl
@@ -1,11 +1,11 @@
 @doc """
 Compute the magnitude of a given vector.
-""" ->
+"""
 function vectorMag(v::Vector)
-    return (sqrt ∘ sum)(v.^2)
-    # could also write: 
-    # v.^2 |> sum |> sqrt 
-    # or simply 
+    return (sqrt ∘ sum)(v .^ 2)
+    # could also write:
+    # v.^2 |> sum |> sqrt
+    # or simply
     # sqrt(sum(v.^2))
 end
 
@@ -17,25 +17,12 @@ function vectorDot(v::Vector, w::Vector)::Float64
 end
 
 @doc """
-Find the angle between the vector and the positive abscissa (x-axis). 
+Find the angle between the vector and the positive x-axis. 
 Output angle can be in either radians or degrees, specified by the 'mode' 
 and is rounded to the nearest decimal.
 """ ->
-function findAngle(v::Vector, mode="rad")
-    try
-        if mode == "deg"
-            return round(atand(v[2], v[1]), digits=1)
-        elseif mode == "rad"
-            return round(atan(v[2], v[1]), digits=1)
-        end
-    catch
-        # error condition
-        if length(v) < 2
-            error("input vector must be 2-dimensional")
-        elseif mode !== "deg" || mode !== "rad"
-            error("input mode not recognized.")
-        else 
-            return 0.0
-        end
-    end
+function findAngle(v::Vector, degMode = false)
+    return length(v) < 2 ? error("input vector must be 2-dimensional") :
+        degMode == true ? round(atand(v[2], v[1]), digits = 1) :
+        round(atan(v[2], v[1]), digits = 1)
 end
